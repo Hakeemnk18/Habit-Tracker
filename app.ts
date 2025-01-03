01c43408be42679cfebd4f8bfbf213a8bb146a9e
Hakeem Muhammed <hakeemnk18@gmail.com>
2025-01-03 15:05:49 +0530

    server setup

--- app.ts
+++ app.ts
@@ -1,13 +1,22 @@
 import express, { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import connectDb from './config/db';
+import router from './routes/roueter';
 connectDb()
 const app =express()
-
 dotenv.config()
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.set("view engine","ejs")
+app.set("views","./views")
 
 const port = process.env.PORT
 
-console.log("server run "+port)
+app.use('/',router)
+
+app.listen(port,()=>{
+    console.log("server is running")
+})
 
